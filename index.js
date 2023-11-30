e22ff001f43e1816be37ac46fac06e9d0073c8e9
georgio893479874 <129616431+georgio893479874@users.noreply.github.com>
2023-11-30 20:03:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import ejs from 'ejs';
+import express from 'express';
+import archiver from 'archiver';
+import { zip } from './zipper.js';
+import multer from 'multer';
+
+const app = express()
+
+function autoRename() {
+    fs.readdir('./', (err, files) => {
+        let filesList = [];
+        for (let i = 0; i < files.length; i++) {
+            if (fs.statSync(path.join('./', files[i])).isFile()) {
+                filesList.push(files[i])
+            }
+        }
+        console.log(filesList)
+        for (let i = 0; i < filesList.length; i++) {
+            let extesion = path.extname(filesList[i])
+            let newName = i+1 + extesion
+            fs.rename('./'+ filesList[i], newName, (err) =>{
+            })
+        }
+    })
+}
+
+app.post('/zip', (req, res) => {
+    zip()
+})
+
+app.get('/', (req, res) => {
+    res.render('main.ejs', {mode: mode})
+})
+
+app.listen(3000, (err) => {
+    console.log('error')
+})
+
 
