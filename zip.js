a44d4279993985b871357918cc91dd2b914207ff
georgio893479874 <129616431+georgio893479874@users.noreply.github.com>
2023-12-23 21:15:51 +0200

    Update zip.js

--- zip.js
+++ zip.js
@@ -2,39 +2,37 @@ import archiver from 'archiver';
 import fs from 'fs';
 import path from 'path';
 
+export async function zip() {
+const output = fs.createWriteStream('./example.zip');
+const archive = archiver('zip', {
+  zlib: { level: 1 }
+});
 
-export function zip() {
-  const output = fs.createWriteStream('./example.zip');
-  const archive = archiver('zip', {
-    zlib: { level: 1 } 
-  });
+archive.directory('./uploads', 'uploads');
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+  return true;
+});
+output.on('end', function() {
+  console.log('Data has been drained');
+});
 
-  archive.file('./1.txt', { name: './1.txt' });
-
-  output.on('close', function() {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-  });
-
-  output.on('end', function() {
-    console.log('Data has been drained');
-  });
-
-  archive.on('warning', function(err) {
-    if (err.code === 'ENOENT') {
-      throw err;
-    } else {
-      throw err;
-    }
-  });
-
-  archive.on('error', function(err) {
+archive.on('warning', function(err) {
+  if (err.code === 'ENOENT') {
     throw err;
-  });
-
-  archive.pipe(output);
-  archive.finalize();
+  } 
+  else {
+    throw err;
+  }
+});
+archive.on('error', function(err) {
+  throw err;
+});
+archive.pipe(output);
+archive.finalize();
 }
 
 zip()
+
 
